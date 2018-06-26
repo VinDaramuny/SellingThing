956cf82f8ea422457e1f0a0bcc1155c0276d5656
vindny <vindaramuny.vd@gmail.com>
2018-06-26 13:59:59 +0700

    update activity signin

---
+++ app/src/main/java/com/androidteam/sellingthing/sellingthing/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.androidteam.sellingthing.sellingthing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vinda on 6/26/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
